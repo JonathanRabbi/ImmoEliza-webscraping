a5197307704deeff85fcece11d61727b9f656481
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-06-26 16:40:13 +0200

    update

--- scrapping_a_file.py
+++ scrapping_a_file.py
@@ -1,11 +1,32 @@
+import requests 
 from bs4 import BeautifulSoup
-import re
-import requests
 import json
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+for_sale = '/search/house/for-sale'
+for_rent = '/search/house/for-rent' 
+
+list_of_districts = ['/brussels' ] #, '/wallonia', '/gent', '/namur', '/hainaut', '/liege , '/antwerp''
+end_url= '/district?countries=BE&page=1&orderBy=relevance'
 
 root_url = 'https://www.immoweb.be/en'
+for i in range(len(list_of_districts)):
+    current_url = root_url+for_rent+list_of_districts[i]+end_url
+    print(current_url)
 
-session=requests.Session()
-a = session.get(root_url)
+    driver = webdriver.Chrome()
+    driver.get(current_url)
+    time.sleep(10)
+
+    search_results = driver.find_elements(By.CLASS_NAME, "search-results__item")
+
+    for result in search_results:
+       result.click()
+       
+       #print(result.text)
+
 
 
