156506993108c2dee31f28ec6b8f2661dbf8790c
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-06-28 05:15:20 +0200

    update

--- scrapping_a_file.py
+++ scrapping_a_file.py
@@ -20,19 +20,24 @@ session = requests.Session()
 cookies = session.cookies
 driver = webdriver.Chrome()
 
-for cookie in cookies:
-    driver.add_cookie({'name': cookie.name, 'value': cookie.value})
-time.sleep(5)
+driver.get(root_url+'/shadow_dom.html')
+time.sleep(3)
+element = driver.execute_script("""return document.querySelector('#usercentrics-root').shadowRoot.querySelector("button[data-testid='uc-accept-all-button']")""")
+element.click()
 
 for i in range(len(list_of_districts)):
     current_link = root_url+for_rent+list_of_districts[i]+end_url
     driver.get(current_link)
+    time.sleep(3)
+
     print(current_link)
 
     search_results = driver.find_elements(By.CLASS_NAME, "search-results__item")
-
+    time.sleep(3)
     for result in search_results:
         result.click()
+        time.sleep(3)
+        
         table = driver.find_elements(By.CLASS_NAME, 'classified-table__row' )
         for x in table:
             print(x.text)
