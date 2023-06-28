e4ed8d1b79bfaf20c746303676c931e3062be695
MythiliPalanisamy <102500645+MythiliPalanisamy@users.noreply.github.com>
2023-06-28 22:34:13 +0200

    data filter for one page

--- cookies.py
+++
@@ -1,5 +0,0 @@
-
-driver.get(root_url)
-time.sleep(3)
-element = driver.execute_script("""return document.querySelector('#usercentrics-root').shadowRoot.querySelector("button[data-testid='uc-accept-all-button']")""")
-element.click()
