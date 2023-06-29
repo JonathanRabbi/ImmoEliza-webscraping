c425958bfc50aa7707ac37034a55ceaa986be579
jonathanrabbi <jonathanrabbi96@gmail.com>
2023-06-29 21:11:12 +0200

    latest pandas working

--- last_version_concurrency.py
+++ last_version_concurrency.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import time
 from concurrent.futures import ThreadPoolExecutor
+import pandas as pd
 
 list_urls = ['https://www.immoweb.be/en/classified/house/for-sale/aywaille%20sougn%C3%A9-remouchamps/4920/10663667', 'https://www.immoweb.be/en/classified/villa/for-sale/aywaille/4920/10661605', 'https://www.immoweb.be/en/classified/villa/for-sale/aywaille/4920/10660061', 'https://www.immoweb.be/en/classified/house/for-sale/aywaille/4920/10660060']
 things = ['Locality', 'Bedrooms', 'Construction year', 'Building condition', 'Number of frontages', 'Outdoor parking space', 'Bathrooms', 'Shower rooms', 'Office', 'Toilets', 'Kitchen type', 'Furnished', 'Terrace', 'Heating type', 'Price', 'External reference', 'Address', 'Energy class', 'Primary energy consumption']
@@ -28,7 +29,7 @@ def home_scrape(url):
         else:
             list_of_data.append(data.contents[0].strip())
 
-    result_dict = {x: y for x, y in zip(list_of_header, list_of_data)}
+    result_dict = {x:y for x, y in zip(list_of_header, list_of_data)}
 
     needed_things = {}
     for x in things:
@@ -37,7 +38,7 @@ def home_scrape(url):
         else:
             needed_things[x] = 0
     return needed_things
-
+    
 with ThreadPoolExecutor(max_workers=10) as executor:
     start = time.time()
     futures = [executor.submit(home_scrape, url) for url in list_urls]
@@ -46,3 +47,5 @@ with ThreadPoolExecutor(max_workers=10) as executor:
     print("Time Taken: {:.6f}s".format(end - start))
     print(results)
 
+df=pd.DataFrame(results)
+df.to_csv('scraped_data.csv', index=False)
